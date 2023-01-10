4c7138da90a9a8a4ba9ecff55387a4a8dbc1b2dd
jeffersonsantos26 <jeffersonaraujo1808@gmail.com>
2023-01-09 21:45:13 -0300

    Criação de primeiros arquivos

---
+++ pacman.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char mapa[5][10+1];
+int linhas;
+int colunas;
+
+
+int main(){
+    
+
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+    if(f==0){
+       printf("Erro na leitura do mapa !\n");
+       exit(1);
+    }
+
+    fscanf(f,"%d %d", &linhas, &colunas);
+    printf("%d linhas, %d colunas\n",linhas, colunas);
+
+    for (int i = 0; i < 5; i++){
+        fscanf(f, "%s", mapa[i]);
+    }
+    
+    for (int i = 0; i < 5; i++){
+        printf("%s\n",mapa[i]);
+    }
+    
+    fclose(f);
+}
